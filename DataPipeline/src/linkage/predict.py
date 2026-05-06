e2afd84f62fe4341a2721d91faa39d3d49480964
morsoletodev <ricmorsoleto@gmail.com>
2026-05-06 13:25:09 -0300

    feat!(predict): use new config logic

--- DataPipeline/src/linkage/predict.py
+++ DataPipeline/src/linkage/predict.py
@@ -4,8 +4,9 @@ import sys
 import pandas as pd
 from splink import DuckDBAPI, Linker
 
-from .. import config as conf
+from ..config import LinkConfig, DatasetConfig, GlobalConfig
 from .train import train_splink
+from .model import _dfs
 
 logger = logging.getLogger(__name__)
 
@@ -14,19 +15,20 @@ def predict_splink():
     """Perform linkage between SIM and SINASC"""
     logger.info("[Linkage Start]")
 
-    try:
-        df_sim = pd.read_parquet(conf.INTERIM_SIM, columns=conf.LINKAGE_COLUMNS)
-        df_sinasc = pd.read_parquet(conf.INTERIM_SINASC, columns=conf.PRE_LINKAGE_COLS)
+    dc = DatasetConfig()
+    lc = LinkConfig()
+    gc = GlobalConfig()
 
+    try:
         # Executes training if needed
-        if not conf.SPLINK_MODEL.exists():
+        if not gc.f_model.exists():
             logger.info("ModelFileNotFound: training model instead")
             train_splink()
             logger.info("Resuming Linkage")
 
         linker = Linker(
-            [df_sinasc[conf.LINKAGE_COLUMNS], df_sim],
-            conf.SPLINK_MODEL,
+            _dfs(lc.link_columns, dc.datasets),
+            gc.f_model,
             db_api=DuckDBAPI(),
         )
 
@@ -38,18 +40,21 @@ def predict_splink():
         sql = f"""
         SELECT DISTINCT unique_id_l
         FROM {df_links.physical_name}
-        WHERE match_weight >= {conf.LINKAGE_THRESHOLD}
+        WHERE match_weight >= {lc.link_threshold}
         """
         id_sinasc = linker.misc.query_sql(sql)["unique_id_l"]
         logger.info("SINASC ids found: %s", id_sinasc.shape)
 
         # Creates target column named `OBITO`
+        df_sinasc = pd.read_parquet(
+            dc.datasets["SINASC"].interim, columns=dc.pre_linkage_cols
+        )
         df_sinasc["OBITO"] = df_sinasc["unique_id"].isin(id_sinasc).astype(int)
         logger.info("'OBITO' column created")
 
         # Saves data
-        df_sinasc[conf.ENSEMBLE_COLUMNS].to_parquet(
-            conf.PROCESSED_SINASC, compression="gzip"
+        df_sinasc[dc.ensemble_columns].to_parquet(
+            dc.datasets["SINASC"].processed, compression="gzip"
         )
         logger.info("Output file for ensemble models created")
 
